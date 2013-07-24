48244e9c570d020e876ad4dd3148000ef1fda86f
Игорь Мищенко <imischenko@itrack.ru>
2013-07-25 00:13:16 +0400

    Initial Commit

---
+++ DropletsManager/IMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IMAppDelegate.h
+//  DropletsManager
+//
+//  Created by Игорь Мищенко on 25.07.13.
+//  Copyright (c) 2013 Igor Mischenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DropletsManager/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DropletsManager
+//
+//  Created by Игорь Мищенко on 25.07.13.
+//  Copyright (c) 2013 Igor Mischenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IMAppDelegate class]));
+    }
+}
